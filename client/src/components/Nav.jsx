41a94cf67be0b00713fc80a25ec852a0ec636020
Kristina <kavdesilva@gmail.com>
2022-08-15 11:52:52 -0400

    home and nav successfully routed in app.js

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,14 @@
+const Nav = () => {
+
+    return (
+        <div>
+            <ul>
+                <li>home</li>
+                <li>login/sign up</li>
+                <li>songs</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+
+    return (
+        <div>
+            <h1>this is the homepage.</h1>
+        </div>
+    )
+}
+
+export default Home
