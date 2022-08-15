e50b09cfbc9613c001c89c49b407983e6997fe6c
Kristina <kavdesilva@gmail.com>
2022-08-15 11:58:29 -0400

    logo added

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,7 +1,10 @@
+import NavLogo from '../assets/NavLogo.svg'
+
 const Nav = () => {
 
     return (
         <div>
+            <div id='logo'><img src={NavLogo} alt="cycle-list-logo" /></div>
             <ul>
                 <li>home</li>
                 <li>login/sign up</li>
