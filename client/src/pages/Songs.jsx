ee6b22e1b66fe1586be74ccff672b6c2d699270b
Kristina <kavdesilva@gmail.com>
2022-08-16 16:40:25 -0400

    songs mapped

--- client/src/pages/Songs.jsx
+++ client/src/pages/Songs.jsx
@@ -9,7 +9,18 @@ const Songs = ({ songs }) => {
       <NavLink to="/songs/:songId">Song/Routine Details</NavLink>
       <NavLink to="/songs/genres">Songs by Genre</NavLink>
       <NavLink to="/songs/routine-types">Songs by Routine Type</NavLink>
-      <div></div>
+      <div className='songs-container'>
+        {
+          songs.map((song) => (
+            <div key={song._id} className='song-card'>
+              <h3>'{song.title}'</h3>
+              <p>{song.artist}</p>
+              <p>{song.genre}</p>
+              <p>{song.length}</p>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
