51a324f8291a7b5926cc762275d672bbf6a5b835
Kristina <kavdesilva@gmail.com>
2022-08-16 09:13:47 -0400

    styling of nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,14 +3,16 @@ import { NavLink, Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
-    <div>
-      <div id="logo">
-        <img src={NavLogo} alt="cycle-list-logo" />
-      </div>
-      <div className="nav-links">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/songs">Songs</NavLink>
-        <NavLink to="/about">About</NavLink>
+    <div className='navBar'>
+      <div className='navContent'>
+        <div id="logo">
+          <img src={NavLogo} alt="cycle-list-logo" />
+        </div>
+        <div className="nav-links">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/songs">Songs</NavLink>
+          <NavLink to="/about">About</NavLink>
+        </div>
       </div>
     </div>
   )
