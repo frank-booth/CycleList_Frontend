3ae1af9c201d806f2df238cadb3c7b09350bf5d7
frank-booth <jfraley519@gmail.com>
2022-08-17 12:08:58 -0400

    connected new song with new routine

--- client/src/components/AddRoutine.jsx
+++ client/src/components/AddRoutine.jsx
@@ -1,54 +1,67 @@
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 import axios from 'axios'
 import { BASE_URL } from '../globals'
 
 const AddRoutine = () => {
+  let navigate = useNavigate()
+  let location = useLocation()
+  let songId = location.state.songId
+  const initialStateRoutine = {
+    content: '',
+    category: ''
+  }
 
-    let navigate = useNavigate()
+  const [routineFormState, setRoutineFormState] = useState(initialStateRoutine)
 
-    const initialStateRoutine = {
-        content: '',
-        category: ''
-    }
+  const handleChangeRoutine = (event) => {
+    setRoutineFormState({
+      ...routineFormState,
+      [event.target.id]: event.target.value
+    })
+  }
 
-    const [routineFormState, setRoutineFormState] = useState(initialStateRoutine)
+  const handleSubmitRoutine = async (event) => {
+    event.preventDefault()
+    let res = await axios.post(
+      `${BASE_URL}/api/routines/${songId}`,
+      routineFormState
+    ) //********/
+    setRoutineFormState(initialStateRoutine)
+    navigate(-2)
+  }
 
-    const handleChangeRoutine = (event) => {
-        setRoutineFormState({...routineFormState, [event.target.id]: event.target.value})
-    }
-
-    const handleSubmitRoutine = async (event) => {
-        event.preventDefault()
-        let res = await axios.post(`${BASE_URL}/api/routines/5`, routineFormState) //********/
-        setRoutineFormState(initialStateRoutine)
-    }
-
-    return (
-        <div>
-            <h1>Add Routine Here</h1>
-            <div className='form-container'>
-                <form onSubmit={handleSubmitRoutine}>
-                    <input id="content" 
-                        type="text" 
-                        onChange={handleChangeRoutine} 
-                        value={routineFormState.content} 
-                        placeholder="content"/>
-                    <select id="category" onChange={handleChangeRoutine} value={routineFormState.category}>
-                        <option value=''>Select Category</option>
-                        <option value='Flat Road'>Flat Road</option>
-                        <option value='Climb'>Climb</option>
-                        <option value='Sprints'>Sprints</option>
-                        <option value='Jumps'>Jumps</option>
-                        <option value='Warm-Up'>Warm-Up</option>
-                        <option value='Cool-Down'>Cool Down</option>
-                        <option value='Other'>Other</option>
-                    </select>
-                    <button type='submit'>Submit</button>
-                </form>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Add Routine Here</h1>
+      <div className="form-container">
+        <form onSubmit={handleSubmitRoutine}>
+          <input
+            id="content"
+            type="text"
+            onChange={handleChangeRoutine}
+            value={routineFormState.content}
+            placeholder="content"
+          />
+          <select
+            id="category"
+            onChange={handleChangeRoutine}
+            value={routineFormState.category}
+          >
+            <option value="">Select Category</option>
+            <option value="Flat Road">Flat Road</option>
+            <option value="Climb">Climb</option>
+            <option value="Sprints">Sprints</option>
+            <option value="Jumps">Jumps</option>
+            <option value="Warm-Up">Warm-Up</option>
+            <option value="Cool-Down">Cool Down</option>
+            <option value="Other">Other</option>
+          </select>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default AddRoutine
--- client/src/components/AddSong.jsx
+++ client/src/components/AddSong.jsx
@@ -4,73 +4,92 @@ import axios from 'axios'
 import { BASE_URL } from '../globals'
 
 const AddSong = () => {
+  let location = useLocation()
+  let navigate = useNavigate()
 
-    let location = useLocation()
-    let navigate = useNavigate()
+  let rider = location.state.rider
 
-    let rider = location.state.rider
+  const initialStateSong = {
+    title: '',
+    artist: '',
+    genre: '',
+    length: '',
+    songImage: ''
+  }
 
-    const initialStateSong = {
-        title: '',
-        artist: '',
-        genre: '',
-        length: '',
-        songImage: ''
-    }
+  const [songFormState, setSongFormState] = useState(initialStateSong)
 
-    const [songFormState, setSongFormState] = useState(initialStateSong)
+  const handleChangeSong = (event) => {
+    setSongFormState({
+      ...songFormState,
+      [event.target.id]: event.target.value
+    })
+  }
 
-    const handleChangeSong = (event) => {
-        setSongFormState({...songFormState, [event.target.id]: event.target.value})
-    }
+  const handleSubmitSong = async (event) => {
+    event.preventDefault()
+    let res = await axios.post(
+      `${BASE_URL}/api/songs/${rider.id}`,
+      songFormState
+    ) //********/
+    setSongFormState(initialStateSong)
+    console.log(res.data.id)
+    navigate(`/songs/${res.data.id}/addroutine`, {
+      state: { songId: res.data.id }
+    })
+  }
 
-    const handleSubmitSong = async (event) => {
-        event.preventDefault()
-        let res = await axios.post(`${BASE_URL}/api/songs/${rider.id}`, songFormState) //********/
-        setSongFormState(initialStateSong)
-        navigate(-1)
-    }
-
-    
-    return (
-        <div>
-            <h1>Add songs here</h1>
-            <div className='form-container'>
-                <form onSubmit={handleSubmitSong}>
-                    <input id="title" 
-                        type="text" 
-                        onChange={handleChangeSong} 
-                        value={songFormState.title} 
-                        placeholder="title"/>
-                    <input id="artist" 
-                        type="text" 
-                        onChange={handleChangeSong} 
-                        value={songFormState.artist} 
-                        placeholder="artist"/>
-                    <select id="genre" onChange={handleChangeSong} value={songFormState.genre}>
-                        <option value=''>Select Genre</option>
-                        <option value='Pop'>Pop</option>
-                        <option value='Rock'>Rock</option>
-                        <option value='Hip-hop'>Hip-hop</option>
-                        <option value='Country'>Country</option>
-                        <option value='EDM'>EDM</option>
-                        <option value='Other'>Other</option>
-                    </select>
-                    <input id="length" 
-                        type="text" 
-                        onChange={handleChangeSong} 
-                        value={songFormState.length} 
-                        placeholder="length"/>
-                    <input id="songImage" 
-                        type="text" 
-                        onChange={handleChangeSong} 
-                        value={songFormState.songImage} 
-                        placeholder="song image"/>
-                    <button type='submit'>Submit</button>    
-                </form>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Add songs here</h1>
+      <div className="form-container">
+        <form onSubmit={handleSubmitSong}>
+          <input
+            id="title"
+            type="text"
+            onChange={handleChangeSong}
+            value={songFormState.title}
+            placeholder="title"
+          />
+          <input
+            id="artist"
+            type="text"
+            onChange={handleChangeSong}
+            value={songFormState.artist}
+            placeholder="artist"
+          />
+          <select
+            id="genre"
+            onChange={handleChangeSong}
+            value={songFormState.genre}
+          >
+            <option value="">Select Genre</option>
+            <option value="Pop">Pop</option>
+            <option value="Rock">Rock</option>
+            <option value="Hip-hop">Hip-hop</option>
+            <option value="Country">Country</option>
+            <option value="EDM">EDM</option>
+            <option value="Other">Other</option>
+          </select>
+          <input
+            id="length"
+            type="text"
+            onChange={handleChangeSong}
+            value={songFormState.length}
+            placeholder="length"
+          />
+          <input
+            id="songImage"
+            type="text"
+            onChange={handleChangeSong}
+            value={songFormState.songImage}
+            placeholder="song image"
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default AddSong
