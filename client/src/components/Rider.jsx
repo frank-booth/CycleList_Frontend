65bf4a7ff4c44ebe9d07ea24a78ad8e6dac8afeb
E132 <107452563+132E@users.noreply.github.com>
2022-08-18 14:26:04 -0500

    rider song genre styled

--- client/src/components/Rider.jsx
+++ client/src/components/Rider.jsx
@@ -25,39 +25,43 @@ const Rider = ({ songs }) => {
 
   return (
     <div>
-      <h1>{rider.userName}</h1>
-      <h1>My Rides</h1>
-      <button onClick={addSong}>Add Song</button>
+      <section className='hero-section'>
+        <h1>Welcome</h1>
+        <h2>{rider.userName}</h2>
+        <button onClick={addSong}>Add Song</button>
+    </section>
+    <div className='page-title'>
+      <h3>My Songs & Routine</h3>
+    </div>
       <div className='list-container'>
-      <div className="column-headers">
-        <h4>Song Image</h4>
-        <h4>Title/Artist</h4>
-        <h4>Genre</h4>
-        <h4>Length</h4>
-        <h4>Category</h4>
-        <h4>Routine</h4>
-        <h4>Edit/Delete</h4>
-      </div>
-      <div className="songs-container">
-        {riderSongs?.map((song) => (
-          <div key={song.id} className="song-card">
-            <img className="song-image" src={song.songImage} />
-            <div className="artist-info">
-              <h3>'{song.title}'</h3>
-              <p>{song.artist}</p>
+        <div className="column-headers">
+          <h4>Song Image</h4>
+          <h4>Title/Artist</h4>
+          <h4>Genre</h4>
+          <h4>Length</h4>
+          <h4>Category</h4>
+          <h4>Routine</h4>
+          <h4>Edit/Delete</h4>
+        </div>
+        <div className="songs-container">
+          {riderSongs?.map((song) => (
+            <div key={song.id} className="song-card">
+              <img className="song-image" src={song.songImage} />
+              <div className="artist-info">
+                <h3>'{song.title}'</h3>
+                <p>{song.artist}</p>
+              </div>
+              <p>{song.genre}</p>
+              <p>{song.length}</p>
+              <p>{song.Routine?.category}</p>
+              <p>{song.Routine?.content}</p>
+              <div className="song-card-buttons">
+                <button onClick={() => editSong(song)}>Edit</button>
+                <button onClick={() => deleteSong(song)}>Delete</button>
+              </div>
             </div>
-            <p>{song.genre}</p>
-
-            <p>{song.length}</p>
-            <p>{song.Routine?.category}</p>
-            <p>{song.Routine?.content}</p>
-            <div className="song-card-buttons">
-              <button onClick={() => editSong(song)}>Edit</button>
-              <button onClick={() => deleteSong(song)}>Delete</button>
-            </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
       </div>
     </div>
   )
